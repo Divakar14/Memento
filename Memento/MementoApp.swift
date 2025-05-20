228839431c301ce78309ec2560bcf5f8981cc285
Divakar <trdiva2608@gmail.com>
2025-05-20 20:57:25 +0530

    Initial Commit

---
+++ Memento/MementoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MementoApp.swift
+//  Memento
+//
+//  Created by Divakar T R on 20/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MementoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
